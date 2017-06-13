cad70bcd5dcbcf231eea66892daac6c408ff67b0
Diego Aguilar <diegojaguilar0131@gmail.com>
2017-06-13 15:27:05 -0700

    four movie titles w/ ticet purchasing options. and times and quantity of tickets being purchased

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,14 @@
+//buisness logic
+function Ticket(time, price, quantity) {
+  this.time = time;
+  this.price = price;
+  this.quantity = quntity;
+}
+
+
+
+
+// user interface logic
 $(document).ready(function() {
-  
+
 });
